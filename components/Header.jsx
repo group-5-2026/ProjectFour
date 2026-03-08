19af6317e73642d0eecd5404df881efe04f27119
nweigelt-dot <nweigelt@charlotte.edu>
2026-03-07 22:28:33 -0500

    Add Header component with basic structure

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import '/Header.css';
 
+class Header extends React.Component {
+  constructor(props) { 
+  super(props);
+  }
+
+render(){
+  return (<div className="container Header">
+    <div className="header">Web Application</div>
+    <div className="gradient"></div>
+  </div>);
+        }
+}
+export default Header;
